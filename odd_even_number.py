cb960f7b76f096ae7627a4fc5d6f356849db97c3
Bahodir Nematullayev <119866702+teobodev@users.noreply.github.com>
2024-01-28 12:32:07 +0500

    Create odd_even_number.py

---
+++ odd_even_number.py
@@ -0,0 +1,13 @@
+# release the even and odd numbers
+# t.me/baxa_akee_2602
+
+print('Enter x: ')
+x = int(input())
+
+def evenandodd (t):
+    if(t%2==0):
+        print('This is even number')
+    else:
+        print('This is odd number')
+evenandodd(x)
+
